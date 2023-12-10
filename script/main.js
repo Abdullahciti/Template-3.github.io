58b0c0fee9ee1d4e86fd9f9db7ec3ed27fa745f8
Top C <136592513+Abdullahciti@users.noreply.github.com>
2023-12-10 12:22:16 +0100

    Add files via upload
    Signed-off-by: Top C <136592513+Abdullahciti@users.noreply.github.com>

--- script/main.js
+++ script/main.js
@@ -1,40 +1,43 @@
-let myclients = document.querySelector(".stats .one");
-let myProjects = document.querySelector(".stats .two");
-let myCountries = document.querySelector(".stats .three");
-let myMoney = document.querySelector(".stats .four");
+// Website Loader & go up Button
 let loader = document.querySelector(".loader");
 let goUpBtn = document.querySelector(".go-up")
-// console.log(myclients, myProjects, myCountries, myMoney, loader);
 
-function startFound (el){
+let maxHeight = document.documentElement.scrollHeight  - document.documentElement.clientHeight;
+
+
+// Stats
+let stats = document.querySelector (".stats");
+let allStatsElements = document.querySelectorAll(".stats .number");
+let skills = document.querySelector(".skills");
+let skillSpan = document.querySelector(".skills .my-percent");
+let skillsSpans = document.querySelectorAll(".skills .my-percent");
+
+// console.log(skills, skillsSpans)
+
+
+// window.onscroll = function(){
+// }
+
+
+// Settings 
+let started = false;
+let startedAgain = false;
+
+function startFind (el){
     let goal = el.dataset.goal;
     let counter = setInterval(() => {
         el.textContent++;
         if(el.textContent === goal || parseInt(el.textContent) >= 135){
             clearInterval(counter);
         }
-    }, 150);
+    }, 1500 / goal);
 }
-// startFound(document.querySelector(".box div .one"));
-// startFound(document.querySelector(".box div .two"));
-// startFound(document.querySelector(".box div .three"));
-// startFound(document.querySelector(".box div .four"));
-// startFound(document.querySelector(".box div .four"));
-// startFound(document.querySelector(".box div .four"));
 
-
-
-
-// Stats
-let maxHeight = document.documentElement.scrollHeight  - document.documentElement.clientHeight
-
-
-window.onscroll = function(){
+window.onscroll = function websiteLoader(){
     let scrollPercentage = (scrollY / maxHeight);
     loader.style.width = `${scrollPercentage * 100}%`;
     if(window.scrollY >= 550){
         goUpBtn.style.display = "block"
-        
         if(window.scrollY >= 551){
             goUpBtn.style.opacity = "1"
             goUpBtn.addEventListener("click", () => {
@@ -43,8 +46,22 @@ window.onscroll = function(){
                     behavior:"smooth"
                 })
             })
-    }}else{
+        }
+    } else{
         goUpBtn.style.display = "none"
         goUpBtn.style.opacity = "0"
     }
+    if (window.scrollY >= (skills.offsetTop - 150)) {
+            let skGoal = skillSpan.dataset.skills;
+            skillsSpans.forEach((e)=> {
+            e.style.width = `${skGoal}%`;
+        })
     }
+    if (window.scrollY >= (stats.offsetTop - 150)) {
+        if (!started){
+            allStatsElements.forEach((ele) => startFind(ele));
+        }
+        started = true;
+    }
+}
+
